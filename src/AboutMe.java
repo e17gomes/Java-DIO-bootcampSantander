07d63ea95131dd55aa6651b4445b51330a5e892d
e17gomesdev <e17gomes@users.noreply.github.com>
2024-05-06 22:00:08 -0300

    new activity

---
+++ src/AboutMe.java
@@ -0,0 +1,9 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        scanner.close();
+    }
+}
--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,6 +1,6 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         final int cinco = 5; //variavel imutavel
-        System.out.println("seila");
+        System.out.println("seila"+ cinco);
     }
 }
